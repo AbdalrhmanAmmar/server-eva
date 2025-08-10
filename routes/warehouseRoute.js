3dac70b056481ebad661fa88838d825bf336dba2
AbdalrhmanAmmar <bodyty2n@gmail.com>
2025-08-10 19:32:07 +0300

    fis:update

--- routes/warehouseRoute.js
+++ routes/warehouseRoute.js
@@ -1,12 +1,10 @@
 import express from 'express';
 import { addWarehouse, getAllWarehouses,getWarehouseById,updateWarehouse } from '../controller/warehouseController.js';
-import { getProductsByWarehouse } from '../controller/productController.js';
 const router = express.Router();
 
 router.post('/', addWarehouse);
 router.get('/', getAllWarehouses);
 router.get('/:id', getWarehouseById);
-router.get('/:warehouse/products', getProductsByWarehouse); 
 router.patch('/:id', updateWarehouse);
 
 export default router;
